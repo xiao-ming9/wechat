54480d8a8ae91ccb92f514f4b69f06f90e0cd87b
xiaoming <934933088@qq.com>
2018-10-17 19:16:39 +0800

    学习微信开发

---
+++ app/Http/Controllers/WeChatController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Log;
+
+class WeChatController extends Controller
+{
+    /**
+     * 处理微信请求消息
+     * 
+     * @return string
+     */
+    public function serve()
+    {
+        Log::info('request arrived.');
+
+        $app = app('wechat.official_account');
+        $app->serve->push(function($message){
+            return "连接成功";
+        });
+
+        return $app->server->serve();
+    }
+}

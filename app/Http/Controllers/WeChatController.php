a8aafc26554f3b069ff683531023af6750b56cba
xiaoming <934933088@qq.com>
2018-10-18 23:24:43 +0800

    学习微信开发

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -28,13 +28,19 @@ class WeChatController extends Controller
         // $app = app('wechat.official_account');
         $this->app->server->push(function($message){
             switch($message['Content']){
-                case '你好':
-                    return '你好';
-                    break;
-                default:
+                case ($message['MsgType'] == 'even'):
                     $openId = $message['FromUserName'];//获取用户的openId
                     $user  = $this->app->user->get($openId);
                     return '欢迎关注'.$user['nickname'];//获取用户的昵称
+                    break;
+                case '你好':
+                    return '你好';
+                    break;
+                case '小铭帅不帅':
+                    return '帅';
+                    break;
+                default:
+                    return '收到你的消息啦';
                     break;
             }
                 //return "欢迎来到小铭的测试号！";
